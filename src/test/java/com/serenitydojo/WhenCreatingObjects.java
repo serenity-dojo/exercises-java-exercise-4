412003f759098bdf8713cd6d0a3a8b5e71212626
John Ferguson Smart <john.smart@wakaleo.com>
2022-08-01 10:57:31 +0100

    Starting point

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -18,7 +18,7 @@ public class WhenCreatingObjects {
 
         String dogSound = "";
         // TODO: implement a method in the Dog sound called makeNoise() that returns the sound a dog makes: "Woof", e.g.
-        dogSound = fido.makeNoise();
+        // dogSound = fido.makeNoise();
 
         Assert.assertEquals(dogSound, "Woof");
     }
@@ -28,7 +28,7 @@ public class WhenCreatingObjects {
         Dog fido = new Dog("Fido","Bone", 5);
 
         // TODO: implement a method in the Dog sound called feed() that sets the isFed variable to true:
-         fido.feed();
+        // fido.feed();
 
         Assert.assertTrue(fido.isFed());
 
